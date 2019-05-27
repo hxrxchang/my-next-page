a9a605d537eacc0072e97d82f5e15c7e60ea3ed8
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-27 23:43:10 +0900

    set static folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,8 @@
-export default () => <h1>Welcome to Next.js for TypeScript!</h1>;
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to Next.js for TypeScript!</h1>
+    <img src="/static/chowder.jpeg" alt="" />
+  </div>
+);
+
+export default IndexPage;

6769d54745cec0f2556cd1d26fcc02431bfbda4f
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-27 23:07:31 +0900

    setting for static page

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
-const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+const withPlugins = require('next-compose-plugins');
+const withTypescript = require('@zeit/next-typescript');
+
+module.exports = withPlugins([withTypescript], {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+    };
+  },
+});

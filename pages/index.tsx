7342b3d56adb794e8bfcf299a34c183b4798e361
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-29 17:27:00 +0900

    install styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-const IndexPage = () => (
-  <div>
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 200px;
+`;
+
+const IndexPage: React.FC<{}> = () => (
+  <Wrapper>
     <h1>Welcome to Next.js for TypeScript!</h1>
     <img src="/static/chowder.jpeg" alt="" />
-  </div>
+  </Wrapper>
 );
 
 export default IndexPage;

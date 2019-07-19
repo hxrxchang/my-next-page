d2c4847355f8f473d4eda1343241f00a74e1b561
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-07-19 19:17:34 +0900

    arrow function

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require('next-compose-plugins');
 const withTypescript = require('@zeit/next-typescript');
 
 module.exports = withPlugins([withTypescript], {
-  exportPathMap: function() {
+  exportPathMap: () => {
     return {
       '/': { page: '/' },
       '/accounts': { page: '/accounts' },

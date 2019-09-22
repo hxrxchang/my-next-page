33267c7aa9fa4590f15549025797a21189bbb596
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-09-22 15:47:38 +0900

    動的にheadを操作するcomponentを作成

---
+++ setting/head.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Head from 'next/head';
+
+interface Props {
+  title: string;
+  description?: string;
+  keyword?: string;
+  image?: string;
+  url?: string;
+}
+
+export default ({ title, description, keyword, image, url }: Props): JSX.Element => {
+  return (
+    <Head>
+      <title key="title">{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta name="keywords" content={keyword} />
+      <meta property="og:type" content="blog" />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:site_name" content={title} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@tcr_jp" />
+      <meta name="twitter:url" content={image} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <link rel="canonical" href={url} />
+      <link rel="shortcut icon" href={'https://t-cr.jp/favicon.ico'} />
+      <link rel="apple-touch-icon" href={'https://t-cr.jp/logo.png'} />
+    </Head>
+  );
+};

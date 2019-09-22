41695023d9d6d87337b83c0c70b3ce56dcf08c8f
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-09-23 00:44:20 +0900

    move head.tsx

--- setting/head.tsx
+++ setting/head.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NextHead from 'next/head';
-import { environment } from './../environments/environment';
+import { environment } from '../environments/environment';
 
 interface Props {
   title: string;
@@ -17,7 +17,7 @@ export default ({ title, description, image, path }: Props): JSX.Element => {
       <meta property="og:description" content={description} />
       <meta property="og:type" content="blog" />
       <meta property="og:url" content={`${environment.url + path}`} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={image || '../static/ogp-profile.jpg'} />
       <meta property="og:site_name" content={title} />
       <link rel="canonical" href={`${environment.url + path}`} />
       <link rel="shortcut icon" href={'https://t-cr.jp/favicon.ico'} />

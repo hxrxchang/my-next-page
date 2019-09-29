6d98072cf0ec17efe0e76c1188dd6923262d4fb4
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-09-30 01:49:32 +0900

    存在しないブログURLを踏んだ時には500じゃなくて404を返す

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next';
+import ErrorPage from 'next/error';
 import { BlogContent } from '../../components';
 
-const BlogPage: NextPage<{ content: string }> = ({ content }) => {
+const BlogPage: NextPage<{ content: string; statusCode: 200 | 404 }> = ({ content, statusCode }) => {
+  if (statusCode === 404) {
+    return <ErrorPage statusCode={statusCode}></ErrorPage>;
+  }
   return (
     <>
       <BlogContent content={content}></BlogContent>
@@ -10,9 +14,12 @@ const BlogPage: NextPage<{ content: string }> = ({ content }) => {
 };
 
 BlogPage.getInitialProps = async (context) => {
-  console.log(context.query.id);
-  const content = await require(`../../docs/blogs/${context.query.id}.md`);
-  return { content: content.default };
+  try {
+    const content = await require(`../../docs/blogs/${context.query.id}.md`);
+    return { content: content.default, statusCode: 200 };
+  } catch (e) {
+    return { content: '', statusCode: 404 };
+  }
 };
 
 export default BlogPage;

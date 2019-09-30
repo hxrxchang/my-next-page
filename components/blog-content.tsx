29b640422491e1bb1c54dbd87dcab1c2e3db6653
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-09-30 09:25:04 +0900

    fix ui

--- components/blog-content.tsx
+++ components/blog-content.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 
+const StyledWrapper = styled.div`
+  padding: 0 10%;
+
+  @media (max-width: 700px) {
+    padding: 0;
+
+    min-height: 300px;
+  }
+`;
+
 export const BlogContent: React.FC<{ content: string }> = ({ content }) => {
-  return <ReactMarkdown source={content} />;
+  return (
+    <StyledWrapper>
+      <ReactMarkdown source={content} />
+    </StyledWrapper>
+  );
 };

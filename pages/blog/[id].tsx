dfae9394c097f23545301365d076bcff0599a0f6
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-10-22 13:05:37 +0900

    temp

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,6 +1,16 @@
+import React from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Icon from '@material-ui/core/Icon';
+import Drawer from '@material-ui/core/Drawer';
+// import Button from '@material-ui/core/Button';
+// import List from '@material-ui/core/List';
+// import Divider from '@material-ui/core/Divider';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import MailIcon from '@material-ui/icons/Mail';
 import styled from 'styled-components';
 import { BlogContent, Layout } from '../../components';
 import Head from '../../setting/head';
@@ -12,7 +22,16 @@ interface Props {
   blogData: BlogData | null;
 }
 
-const StyledWrapper = styled.div`
+const StyledList = styled.div`
+  width: 100%;
+`;
+const SideList = () => (
+  <StyledList>
+    <p>hoge</p>
+  </StyledList>
+);
+
+const StyledPage = styled.div`
   .menu-icon {
     display: none;
   }
@@ -25,6 +44,8 @@ const StyledWrapper = styled.div`
 `;
 
 const BlogPage: NextPage<Props> = ({ content, statusCode, blogData }) => {
+  const [isSidenavOpen] = React.useState(true);
+
   if (statusCode === 404) {
     const e: any = new Error();
     e.code = 'ENOENT';
@@ -34,14 +55,17 @@ const BlogPage: NextPage<Props> = ({ content, statusCode, blogData }) => {
   return (
     <>
       <Head title={blogData!.title} page={router.asPath} description={blogData!.description} type="website"></Head>
-      <StyledWrapper>
+      <StyledPage>
         <div className="menu-icon">
           <Icon>menu</Icon>
         </div>
+        <Drawer open={isSidenavOpen}>
+          <SideList></SideList>
+        </Drawer>
         <Layout route={router.route}>
           <BlogContent content={content}></BlogContent>
         </Layout>
-      </StyledWrapper>
+      </StyledPage>
     </>
   );
 };

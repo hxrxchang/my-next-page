37cb2fe81794fb891f1e532f52da1553b49b894e
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-03-11 02:06:49 +0900

    コメントアウトしていたnext-configのpath指定を削除

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 const withPlugins = require('next-compose-plugins');
 
-// const blogIdList = ['hello-world'];
-
 module.exports = withPlugins([], {
-  // exportPathMap: () => {
-  //   const pathMap = {
-  //     '/': { page: '/' },
-  //     '/accounts': { page: '/accounts' },
-  //     '/works': { page: '/works' },
-  //     '/blogs': { page: '/blogs' },
-  //   };
-
-  //   blogIdList.forEach((blogId) => {
-  //     pathMap[`/blog/${blogId}`] = { page: '/blog/[id]', query: { id: blogId } };
-  //   });
-  //   return pathMap;
-  // },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,

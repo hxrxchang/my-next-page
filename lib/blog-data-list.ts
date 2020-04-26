4e583010286d97f5e88496240ebaf965f43fe37b
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-04-27 01:51:46 +0900

    remove unnecessary file

--- lib/blog-data-list.ts
+++
@@ -1,5 +0,0 @@
-import { BlogData } from '../src/models';
-
-export const blogDataList: BlogData[] = [
-  { id: 'hello-world', title: 'ブログ作りました', description: 'ブログ作りました', createdAt: '2019-10-04', updatedAt: '2020-04-21' },
-];

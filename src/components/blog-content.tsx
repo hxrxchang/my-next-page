f8fece8ee8ed6b81c4bfb104954ce55c439abfd9
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-27 02:11:14 +0900

    yarn format

--- src/components/blog-content.tsx
+++ src/components/blog-content.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm'
+import gfm from 'remark-gfm';
 import { CodeBlock } from './code-block';
 import { EmbedType } from '../models';
 import { isTwitterEmbed } from '../domains/blog';

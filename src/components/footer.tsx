9c31769a40b556fb7ccdc56671aa9debe11c075f
hxrxchang <mail@hxrxchang.dev>
2022-01-18 00:41:06 +0900

    feat: RSS feedへのリンクを用意

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,17 +4,24 @@ import styled from 'styled-components';
 const Styled = styled.footer`
   border-top: 1px solid #efefef;
   text-align: center;
+
+  .link {
+    padding: 10px;
+  }
 `;
 
 export const Footer: React.FC<{}> = ({}) => {
   return (
     <Styled>
-      <p>
-        Source code is{' '}
-        <a href="https://github.com/hxrxchang/my-next-page" target="_blank" rel="noopener">
-          here.
+      <div>
+        <a className="link" href="https://github.com/hxrxchang/my-next-page" target="_blank" rel="noopener">
+          Source code
         </a>
-      </p>
+        <span>|</span>
+        <a className="link" href="/feed.xml" target="_blank" rel="noopener">
+          RSS feed
+        </a>
+      </div>
       <p>&copy; 2019 hxrxchang</p>
     </Styled>
   );
